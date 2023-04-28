b4e646799bfae4b3b7f7ce69e8d1d9caa5809f16
Angelika Wicka <wicka.angelika@gmail.com>
2023-04-28 20:55:21 +0200

    Exercise 3

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -6,11 +6,13 @@ import { welcomeViewkRouterMetaData } from './WelcomeView/router-data';
 
 import { SubRouteExercise1 } from './Exercise1/router-data';
 import { SubRouteExercise2 } from './Exercise2/router-data';
+import { SubRouteExercise3 } from './Exercise3/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   welcomeViewkRouterMetaData,
   SubRouteExercise1,
   SubRouteExercise2,
+  SubRouteExercise3,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
