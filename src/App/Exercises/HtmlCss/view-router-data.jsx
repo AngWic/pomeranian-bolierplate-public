a27455fe07b8af8b4ed04e69af0312ac087df3d0
Angelika Wicka <wicka.angelika@gmail.com>
2023-05-10 15:04:28 +0200

    Exercises with html and css for adding img

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -1,6 +1,10 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
+import { blockRouterMetaData as blockRouterMetaData09052023 } from './Exercise-example-09-05-2023/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231];
+export const blockRouterMetaData = [
+  blockRouterMetaData121220231,
+  ...blockRouterMetaData09052023,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
