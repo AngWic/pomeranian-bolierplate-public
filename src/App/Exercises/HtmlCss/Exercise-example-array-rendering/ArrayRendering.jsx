8aa7dca9c077499b58bd0c061e478a3ab223f1af
Angelika Wicka <wicka.angelika@gmail.com>
2023-05-11 20:00:01 +0200

    Array rendering

---
+++ src/App/Exercises/HtmlCss/Exercise-example-array-rendering/ArrayRendering.jsx
@@ -0,0 +1,26 @@
+import './styles.css';
+
+export const ArrayRendering = () => {
+  const cars = [
+    { id: 1, make: 'Saab', model: '92B', year: 1953, owner: 'Jon' },
+    { id: 2, make: 'Volvo', model: 'C40', year: 2021, owner: 'Jenny' },
+    { id: 3, make: 'Audi', model: 'A3', year: 2019, owner: 'Michael' },
+    { id: 4, make: 'Tesla', model: 'Model S', year: 2022, owner: 'Sarah' },
+  ];
+
+  return (
+    <div>
+      Auta:
+      <ul>
+        {cars.map((car) => {
+          return (
+            // <li key={car.id}>
+            <li key={car.make + car.model}>
+              {car.make} <b>{car.model}</b> ({car.year}) - <u>{car.owner}</u>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
