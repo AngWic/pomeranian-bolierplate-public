1190e98057969add1704f0b3ce544141d9b6cdc7
Angelika Wicka <wicka.angelika@gmail.com>
2023-05-17 17:38:13 +0200

    de Morgan example

--- src/App/Exercises/Js/Boolean/Boolean.jsx
+++ src/App/Exercises/Js/Boolean/Boolean.jsx
@@ -9,11 +9,31 @@ for (let i = 1; i <= 5; i++) {
 console.log(result);
 
 export function BooleanExercises() {
-  const value1 = false;
-  const value2 = true;
-  const value3 = false;
+  const p = true;
+  const q = false;
 
-  const condition = value1 && !value2 && value3;
+  const condition1 = !(p && q);
+  const condition2 = !p || !q;
 
-  return <div>Operatory logiczne {condition && <h1>tajny tekst</h1>}</div>;
+  console.log(condition1 === condition2);
+
+  return <div>Operatory logiczne {condition1 && <h1>tajny tekst</h1>}</div>;
 }
+
+// let result = 10;
+
+// for (let i = 1; i <= 5; i++) {
+//   result = result + 1;
+// }
+
+// console.log(result);
+
+// export function BooleanExercises() {
+//   const value1 = false;
+//   const value2 = true;
+//   const value3 = false;
+
+//   const condition = value1 && !value2 && value3;
+
+//   return <div>Operatory logiczne {condition && <h1>tajny tekst</h1>}</div>;
+// }
