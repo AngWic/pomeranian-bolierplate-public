58bc1a32827a6d4cd94e548c5d8b84fc2300b2b8
Angelika Wicka <wicka.angelika@gmail.com>
2023-05-18 16:41:42 +0200

    Math exercises and If exercises

--- src/App/Exercises/Js/Boolean/Boolean.jsx
+++ src/App/Exercises/Js/Boolean/Boolean.jsx
@@ -1,10 +1,128 @@
 import './styles.css';
-
-console.log(6 % 2);
+import { useState } from 'react';
 
 export function BooleanExercises() {
-  return <div>Operatory logiczne</div>;
+  const [number, setNumber] = useState(0);
+
+  const handleChange = (event) => {
+    setNumber(event.target.value);
+  };
+
+  const circleArea = (number, pi) => {
+    if (number < 1) {
+      return 'Liczba musi być większa od 0';
+    } else if (number > 10) {
+      return 'Liczba nie może być większa niż 10';
+    } else if (number % 2 === 0) {
+      return Math.PI * number * number;
+      // Math.PI * Math.pow(number, 2)?????;
+    } else {
+      return 'Liczba musi być parzysta';
+    }
+  };
+
+  console.log(circleArea);
+
+  return (
+    <div>
+      <p>Wpisz liczbę parzystą w przedziale od 1 do 10:</p>
+      <br />
+      <input type="number" value={number} onChange={handleChange} />
+      <p>Pole koła wynosi: {circleArea}</p>
+      {/* <p>
+        Function <b>canDrive</b> result:
+        <p>{canDrive(age, hasDriverLicense, hasCar)}</p>
+      </p>
+      <hr />
+      <p>
+        Function <b>canDrive2</b> result:
+        <p>{canDrive2(age, hasDriverLicense, hasCar)}</p>
+      </p> */}
+    </div>
+  );
 }
+
+// let age = 18;
+// let hasDriverLicense = true;
+// let hasCar = true;
+
+// function canDrive(age, hasDriverLicense, hasCar) {
+//   if (age >= 18) {
+//     if (hasDriverLicense) {
+//       if (hasCar) {
+//         return 'You can drive your car!';
+//       } else {
+//         return 'You can drive a rental car.';
+//       }
+//     } else {
+//       return "You can't drive without a driver's license.";
+//     }
+//   } else {
+//     return 'You are too young to drive.';
+//   }
+// }
+
+// const canDrive2 = (age, hasDriverLicense, hasCar) => {
+//   if (age < 18) {
+//     return 'You are too young to drive.';
+//   }
+//   if (!hasDriverLicense) {
+//     return "You can't drive without a driver's license.";
+//   }
+//   if (hasDriverLicense) {
+//     return 'You can drive a rental car.';
+//   }
+//   if (hasCar) {
+//     return 'You can drive your car!';
+//   }
+// };
+
+//SWITCH NIE ZADZIAŁA PRZY WIĘCEJ NIŻ JEDNEJ ZMIENNEJ
+// let result = true;
+
+// const driversAge = 18;
+// const driversLicense = true;
+// const driversCar = true;
+
+// switch ((driversAge, driversLicense, driversCar)) {
+//   case 1:
+//     result = 'You can drive your car!';
+//     break;
+//   case 2:
+//     result = 'You can drive a rental car.';
+//     break;
+//   case 3:
+//     result = "You can't drive without a driver's license.";
+//     break;
+//   default:
+//     result = 'You are too young to drive.';
+//     break;
+// }
+
+// console.log(result);
+
+//SWITCH NIE ZADZIAŁA PRZY WIĘCEJ NIŻ JEDNEJ ZMIENNEJ
+
+//SWITCH ZADZIAŁA:
+// let result = '';
+// const animal = 'horse';
+
+// if (animal === 'cat') {
+//   result = 'Wybrane zwierzę to kot';
+// } else if (animal === 'dog') {
+//   result = 'Wybrane zwierzę to pies';
+// } else if (animal === 'mouse') {
+//   result = 'Wybrane zwierzę to mysz';
+// } else {
+//   result = 'Nie znam tego zwierzęcia';
+// }
+
+// console.log('Result: ', result);
+
+//ALBO
+
+// console.log(6 % 2);
+// console.log(Math.random(3));
 
 // export function BooleanExercises() {
 //   const value1 = 3;
