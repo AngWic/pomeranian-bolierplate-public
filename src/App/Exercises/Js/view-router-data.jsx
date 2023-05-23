166b0d5ac7398d1d577b4f0544d97e145fe86df6
Angelika Wicka <wicka.angelika@gmail.com>
2023-05-23 21:35:58 +0200

    View router data update - few last days

--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -3,10 +3,22 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { blockRouterMetaData as blockRouterMetaDataJsVariables1 } from './Exercise-js-variables-1/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exercise-js-variables-2/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
+import { emptyValuesAndCommentskRouterMetaData as emptyValuesAndCommentskRouterMetaData } from './EmptyValuesAndComments/router-data';
+import { dateTimeRouterMetaData as dateTimeRouterMetaData } from './DateTime/router-data';
+import { arrayRouterMetaData as arrayRouterMetaData } from './Array/router-data';
+import { jsFunctionsRouterMetaData as jsFunctionsRouterMetaData } from './JsFunctions/router-data';
+// import { blockRouterMetaData as blockRouterMetaDataWarmUp22 } from './Blok22WarmUp/router-data';
+import { blockRouterMetaData as blockRouterMetaDataSetTimeOut } from './SetTimeout/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
   blockRouterMetaDataJsVariables2,
   blockRouterMetaDataJsFunction1,
+  emptyValuesAndCommentskRouterMetaData,
+  dateTimeRouterMetaData,
+  arrayRouterMetaData,
+  jsFunctionsRouterMetaData,
+  // blockRouterMetaDataWarmUp22,
+  blockRouterMetaDataSetTimeOut,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
