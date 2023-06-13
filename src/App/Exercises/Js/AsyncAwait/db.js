39e9dc3bb1e55ec41f0fbb013b10bbef9ef8043d
Angelika Wicka <wicka.angelika@gmail.com>
2023-06-13 21:37:21 +0200

    Exercises with Promises, async, await, try... catch

---
+++ src/App/Exercises/Js/AsyncAwait/db.js
@@ -0,0 +1,29 @@
+import { createRandomCompany, createRandomUser } from './utils';
+
+
+export const companies = Array(5)
+  .fill()
+  .map((el) => createRandomCompany());
+
+
+export const employees = Array(25)
+  .fill()
+  .map((el) => createRandomUser(companies[Math.floor(Math.random() * companies.length)].id));
+
+export const getAllEmployes = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve(employees));
+  }, 3000);
+}
+
+export const getAllCompanies = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve(companies));
+  }, 3000);
+}
+
+export const getCompanyDetails = (id) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve(companies.filter((company) => company.id === id)), 3000);
+  }, 3000);
+};
--- src/App/Exercises/Js/AsyncAwait/router-data.jsx
+++ src/App/Exercises/Js/AsyncAwait/router-data.jsx
@@ -3,7 +3,7 @@ import { AsyncAwait } from './AsyncAwait';
 export const blockRouterMetaData = {
   path: 'async-await',
   date: '13-06-2023',
-  linkLabel: 'AsyncAwait',
+  linkLabel: 'AsyncAwait', 
   blockNo: 35,
   element: <AsyncAwait />,
   tags: ['async', 'await', 'asynchronous', 'javascript'],
