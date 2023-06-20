8f7d056a8dd8231d8b1278f945d7662a5b51cfd6
Angelika Wicka <wicka.angelika@gmail.com>
2023-06-20 21:12:28 +0200

    Refreshing the list after adding next todo, possibility to edit a todo, extra pop-up with validation on content / NOT FINISHED YET

--- src/App/Exercises/ReactExercises/TodoList/TodoForm/TodoForm.jsx
+++ src/App/Exercises/ReactExercises/TodoList/TodoForm/TodoForm.jsx
@@ -1,43 +1,125 @@
 import './todoform.css';
 import { requestHandler } from '../requestHandler';
+import { useState } from 'react';
 
+export const TodoForm = ({ hide, getTodos }) => {
+  const [message, setMessage] = useState();
+  const [showPopup, setShowPopup] = useState(false);
 
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [note, setNote] = useState('');
 
-export const TodoForm = ({ hide }) => {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        
-        requestHandler('POST', undefined, {
-            title: e.target.title.value,
-            author: e.target.author.value,
-            note: e.target.note.value,
-        })
-        .then((jsonResponse) => {
-        console.log(jsonResponse);
-        })
-        .catch((errorMessage) => {
-        console.log(errorMessage);
-        });
+  console.log(title);
+  console.log(author);
+  console.log(note);
 
-    };
-    
+  console.log('showPopup', showPopup);
 
+  const createNewTodo = async () => {
+    requestHandler('POST', undefined, {
+      title: title,
+      author: author,
+      note: note,
+    })
+      .then((response) => {
+        console.log(response);
+        setMessage(`Dodano nowe zadanie do listy: "${response.title}"`); //pokazać, że dodano nowego todo
+      })
+      .catch((errorMessage) => {
+        setMessage('Wystąpił błąd, spróbuj ponownie. Błąd: ' + errorMessage);
+      });
+  };
 
-    return <div className="todo-form">
-        Dodawanie zadania
+  const handleSubmit = (e) => {
+    e.preventDefault(); //domyślnie formularz odświeża(przeładowuj) stronę i dodaje parametry do adresu strony, a w React potrzebujemy tego narzędzia do dynamicznego zmieniania aplikacji
+
+    if (title !== '' && author.length > 0 && note === '') {
+      setShowPopup(true);
+    }
+
+    if (title !== '' && author.length !== '' && note !== '') {
+      createNewTodo();
+    }
+  };
+
+  function backToTodoList() {
+    hide(false);
+    getTodos();
+  }
+
+  return (
+    <div className="todo-form">
+      <div className="todo-form-back" onClick={() => backToTodoList()}>
+        &#8592;Wstecz
+      </div>
+
+      <hr />
+
+      <h3>Dodawanie zadania</h3>
+
+      <form onSubmit={handleSubmit}>
+        <h4>Tytuł:</h4>
+        <input
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+          //   placeholder="Kupić parasol na balkon..."
+          type="text"
+        />
+        <br />
+        <h4>Autor:</h4>
+        <input
+          value={author}
+          onChange={(event) => {
+            setAuthor(event.target.value);
+          }}
+          //   placeholder="Wojtek..."
+          type="text"
+        />
+        <br />
+        <h4>Treść:</h4>
+        <textarea
+          value={note}
+          onChange={(event) => setNote(event.target.value)}
+          //   placeholder="Zmierzyć ile mamy miejsca na balkonie od barierki do kanapy i ile musi mieć max średnicy - miarka!..."
+          type="text"
+          cols={50}
+          rows={10}
+        />
+        <br />
+
+        <button
+          createNewTodo={createNewTodo}
+          className="todo-button-add"
+          type="submit"
+        >
+          Dodaj
+        </button>
+
         <hr />
-        <div className="todo-form-back" onClick={() => hide(false)}>&#8592;Wstecz</div>
-        <form onSubmit={handleSubmit}>
-            <h4>Tytuł:</h4>
-            <input placeholder='Kupić parasol na balkon...' name='title' type='text'/><br />
-            <h4>Autor:</h4>
-            <input placeholder='Wojtek...' name='author' type='text'/><br />
-            <h4>Treść:</h4>
-            <textarea 
-            placeholder='Zmierzyć ile mamy miejsca na balkonie od barierki do kanapy i ile musi mieć max średnicy - miarka!...' 
-            name='note' 
-            type='text'/><br />
-            <button type="submit">Dodaj</button>
-        </form>
+
+        {message && <p className="form-message-box">{message}</p>}
+      </form>
+
+      {showPopup && (
+        <div className="todo-form-popup">
+          <div className="todo-form-popup-content">
+            <div>Czy chcesz dodać zadanie bez treści?</div>
+            <br />
+            <button
+              onClick={() => {
+                createNewTodo();
+                setShowPopup(false);
+              }}
+            >
+              Tak
+            </button>
+            <button onClick={() => setShowPopup(false)}>Nie</button>
+          </div>
         </div>
-}
+      )}
+    </div>
+  );
+};
--- src/App/Exercises/ReactExercises/TodoList/TodoList.jsx
+++ src/App/Exercises/ReactExercises/TodoList/TodoList.jsx
@@ -4,11 +4,14 @@ import { TodoItem } from './TodoItem/TodoItem';
 import { TodoForm } from './TodoForm/TodoForm';
 import { requestHandler } from './requestHandler';
 
-
 function CatErrorImg() {
   return (
-    <div style={{margin: '50px'}}>
-      <img src="https://http.cat/images/500.jpg" alt="internal server error cat" style={{width: '500px'}}/>
+    <div style={{ margin: '50px' }}>
+      <img
+        src="https://http.cat/images/500.jpg"
+        alt="internal server error cat"
+        style={{ width: '500px' }}
+      />
     </div>
   );
 }
@@ -19,110 +22,118 @@ export const TodoList = () => {
   const [error, setError] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const [showCreateForm, setShowCreateForm] = useState(false);
-  
+  const [editObject, setEditObject] = useState();
+
   async function getTodos() {
+    setIsLoading(true);
 
-  setIsLoading(true);
+    // function setTodoListWhenOk(json) {
+    //   setTodos(json);
+    // }
+    // requestHandler(setTodoListWhenOk);
+    // to samo można zapisać:
 
+    requestHandler('GET')
+      .then((jsonResponse) => {
+        setTodos(jsonResponse);
+      })
+      .catch((errorMessage) => {
+        setError(errorMessage);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
 
-  // function setTodoListWhenOk(json) {
-  //   setTodos(json);
-  // }
-  // requestHandler(setTodoListWhenOk);
-  // to samo można zapisać:
+    //tutaj jesteśmy, zanim zostanie wysłane zapytanie o dane
+    // const response = await fetch('http://localhost:3333/api/todo');
+    // const getJsonData = await response.json();
+    // console.log(todos);
 
-  requestHandler('GET')
-    .then((jsonResponse) => {
-      setTodos(jsonResponse);
-    })
-    .catch((errorMessage) => {
-      setError(errorMessage);
-    })
-    .finally(() => {
-      setIsLoading(false);
-    });
-    
-  //tutaj jesteśmy, zanim zostanie wysłane zapytanie o dane
-  // const response = await fetch('http://localhost:3333/api/todo');
-  // const getJsonData = await response.json();
-  // console.log(todos);  
-  
-   
-  // //tutaj jesteśmy, gdy otrzymujemy dane
+    // //tutaj jesteśmy, gdy otrzymujemy dane
 
-  // if (response.status === 200) {
-  //   setTodos(getJsonData);
-  // } 
-  
-  // if (response.status !==200 && getJsonData.message) {
-  //   setError(getJsonData.message);
-  // }
-  
-  // if (getJsonData.message) {
-  //     setError(getJsonData.message);
-  //   } else {
-  //     setTodos(getJsonData);
-      
-  //  }
-  
-  };
+    // if (response.status === 200) {
+    //   setTodos(getJsonData);
+    // }
 
- 
+    // if (response.status !==200 && getJsonData.message) {
+    //   setError(getJsonData.message);
+    // }
+
+    // if (getJsonData.message) {
+    //     setError(getJsonData.message);
+    //   } else {
+    //     setTodos(getJsonData);
+
+    //  }
+  }
+
   useEffect(() => {
     getTodos();
   }, []);
 
   if (showCreateForm) {
     return (
-    <div className='todo-list'>
-      <TodoForm hide={setShowCreateForm}/>
-    </div>
+      <div className="todo-list">
+        <TodoForm hide={setShowCreateForm} getTodos={getTodos} />
+      </div>
     );
   }
 
-  
-  return (<div><h3>TODO</h3>
+  return (
+    <div>
+      <h3>TODO</h3>
+      <div>Tutaj znajdziesz listę swoich zadań.</div>
 
-    {error && (
-      <h1>
-      Oh-o! Something went wrong...
-      <br />
-      {error}
-      <br />
-      <button className='todo-list-button' onClick={() => {
-        //gdy kliknięty...
-        setError(false);
-        getTodos();        
-      }}>
-        Try again
-      </button>
-
-      <CatErrorImg />
-      
-    </h1>
+      {error && (
+        <h1>
+          Oh-o! Something went wrong...
+          <br />
+          {error}
+          <br />
+          <button
+            className="todo-list-button"
+            onClick={() => {
+              //gdy kliknięty...
+              setError(false);
+              getTodos();
+            }}
+          >
+            Try again
+          </button>
+          <CatErrorImg />
+        </h1>
       )}
 
-    {isLoading && <p>Ładowanie...</p>}
-  
-    {!error && 
-      todos.map(( {author, createdAt, doneDate, id, isDone, note, title}) => {
-      return (
-        <TodoItem
-        key={id} 
-        author={author}
-        createdAt={createdAt}
-        doneDate={doneDate}
-        id={id}
-        isDone={isDone}
-        note={note}
-        title={title}
-        getTodos={getTodos}
-        
-        />
-      )
-    })}
-    <button onClick={() => setShowCreateForm(true)}>Dodaj</button>
-    {showCreateForm && <TodoForm />}
-  </div>
+      {isLoading && <p>Ładowanie...</p>}
+
+      {!error &&
+        todos.map(
+          ({ author, createdAt, doneDate, id, isDone, note, title }) => {
+            return (
+              <TodoItem //obiekt 'dziecko', do którego przekazujemy propsy //powyżej^ w {}
+                key={id}
+                author={author}
+                createdAt={createdAt}
+                doneDate={doneDate}
+                id={id}
+                isDone={isDone}
+                note={note}
+                title={title}
+                getTodos={getTodos}
+                setEditObject={setEditObject}
+                setShowCreateForm={setShowCreateForm}
+              />
+            );
+          }
+        )}
+      <button
+        className="todo-button-add"
+        onClick={() => setShowCreateForm(true)}
+      >
+        Dodaj
+      </button>
+
+      {showCreateForm && <TodoForm />}
+    </div>
   );
 };
