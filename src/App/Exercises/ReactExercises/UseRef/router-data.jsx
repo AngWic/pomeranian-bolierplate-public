7091491a8c8f8000adeb58896488a1e1c07cfc1b
Angelika Wicka <wicka.angelika@gmail.com>
2023-06-21 21:13:36 +0200

    React useRef examples

---
+++ src/App/Exercises/ReactExercises/UseRef/router-data.jsx
@@ -0,0 +1,10 @@
+import { UseRef } from './UseRef';
+
+export const useRefRouterMetaData = {
+  path: 'react-use-ref-exercise',
+  date: '21-06-2023',
+  linkLabel: 'UseRef',
+  blockNo: 36,
+  element: <UseRef />,
+  tags: ['useRef', 'react', 'hook', 'javascript'],
+};
