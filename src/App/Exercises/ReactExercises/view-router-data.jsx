9582494f0e497d74b0eb4b3f75eea175da87d600
Angelika Wicka <wicka.angelika@gmail.com>
2023-07-05 18:11:29 +0200

    view-router-data conflict resolving???

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -2,18 +2,19 @@ import { SubRouteExercise1 } from './Exercise1/router-data';
 import { SubRouteExercise2 } from './Exercise2/router-data';
 import { SubRouteExercise3 } from './Exercise3/router-data';
 
+import { welcomeViewRouterMetaData } from './WelcomeView/router-data';
+
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
-
-import { welcomeViewRouterMetaData } from './WelcomeView/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExercise1,
   SubRouteExercise2,
   SubRouteExercise3,
 
-  SubRouteExampleMetaData,
   welcomeViewRouterMetaData,
+
+  SubRouteExampleMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
