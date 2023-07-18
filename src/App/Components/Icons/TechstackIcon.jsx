68f25ef9ffecf02990bfd219333a8efdd17a3691
Angelika Wicka <wicka.angelika@gmail.com>
2023-07-18 18:39:32 +0200

    App aside and app footer update

---
+++ src/App/Components/Icons/TechstackIcon.jsx
@@ -0,0 +1,42 @@
+export function TechstackIcon({ className = '' }) {
+  return (
+    <svg
+      width="24"
+      height="25"
+      viewBox="0 0 24 25"
+      fill="none"
+      stroke="none"
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9.00005 13.5C8.34005 13.83 7.79005 14.32 7.38005 14.93C7.15005 15.28 7.15005 15.72 7.38005 16.07C7.79005 16.68 8.34005 17.17 9.00005 17.5"
+        stroke="#004286"
+        stroke-width="1.68301"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M15.2101 13.5C15.8701 13.83 16.4201 14.32 16.8301 14.93C17.0601 15.28 17.0601 15.72 16.8301 16.07C16.4201 16.68 15.8701 17.17 15.2101 17.5"
+        stroke="#004286"
+        stroke-width="1.68301"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M8.99994 22.5H14.9999C19.9999 22.5 21.9999 20.5 21.9999 15.5V9.5C21.9999 4.5 19.9999 2.5 14.9999 2.5H8.99994C3.99994 2.5 1.99994 4.5 1.99994 9.5V15.5C1.99994 20.5 3.99994 22.5 8.99994 22.5Z"
+        stroke="#004286"
+        stroke-width="1.68301"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M2.23004 8.51L21.45 8.5"
+        stroke="#004286"
+        stroke-width="1.68301"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
--- src/App/Layout/AppFooter.jsx
+++ src/App/Layout/AppFooter.jsx
@@ -1,6 +1,6 @@
 import './styles/footer.css';
 
-const email = 'angelika.wicka@pomeranianstartit.pl';
+const email = 'angelika.wicka@pomeranianstartit.com';
 const phone = '+48-695-021-070';
 
 export function AppFooter() {
